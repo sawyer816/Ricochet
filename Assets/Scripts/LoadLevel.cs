9bf0ee57db8e8b4bbe0a59e10dc8b4efaaf06c4a
Rogelio Schevenin Jr <rogelio@schevenin.com>
2022-10-16 14:28:22 -0700

    Fixed player spawning

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class LoadLevel : MonoBehaviour
 {
     [Header("Game Objects")]
+    
+    public GameObject TankPrefab;
     public GameObject playerOne;
     public GameObject playerTwo;
 
@@ -12,13 +14,13 @@ public class LoadLevel : MonoBehaviour
     void Start()
     {
         // create player 1 with keyboard input
-        Instantiate(playerOne, new Vector3(0, 0, 0), Quaternion.identity);
-
-        // rigid body
-        playerOne.AddComponent<Rigidbody>();
-        
-        // player settings
+        playerOne = Instantiate(TankPrefab, new Vector3(0, 0, 0), Quaternion.identity);
         playerOne.AddComponent<Player>();
+        playerOne.GetComponent<Player>().playerOne = true;
+
+        // create player 2 with keyboard input
+        playerTwo = Instantiate(TankPrefab, new Vector3(5, 0, 0), Quaternion.identity);
+        playerTwo.AddComponent<Player>();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public bool primary = true; // player 1 or 2
-    public bool KeyboardInput = true; // keyboard input
-    public float Velocity = 3f; // default movement speed
+    public bool playerOne;
+    public float Velocity = 10f;
 
     // called before first frame
     void Start()
     {
+
     }
 
     // called once per frame
     void Update()
     {
 
-        // player input handling
-        if (KeyboardInput) {
+        // player one input handling
+        if (playerOne) {
             if (Input.GetKey(KeyCode.W)) {
                 transform.Translate(Vector3.forward * Time.deltaTime * Velocity);
             }
             if (Input.GetKey(KeyCode.S)) { 
                 transform.Translate(-1 * Vector3.forward * Time.deltaTime * Velocity);
             }
-            if (Input.GetKey(KeyCode.A)) { 
+            if (Input.GetKey(KeyCode.A)) {
                 transform.Rotate(0, -0.5f, 0);
             }
             if (Input.GetKey(KeyCode.D)) {
@@ -33,10 +33,24 @@ public class Player : MonoBehaviour
             }
         }
 
-        // controller input handling
+        // player two input handling
         else {
-
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * Velocity);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(-1 * Vector3.forward * Time.deltaTime * Velocity);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Rotate(0, -0.5f, 0);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Rotate(0, 0.5f, 0);
+            }
         }
-        
     }
 }
