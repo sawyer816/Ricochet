c6f0e7ed62b344c08ee5691d10bc8f1b0ddd4ffb
sawyer816 <sawyer816@gmail.com>
2022-12-15 12:47:15 -0800

    Delete Oldest Shots

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -9,24 +9,32 @@ public class Shoot : MonoBehaviour
     public Rigidbody shell;
     public Transform fire;
     public bool playerOne;
+    public Rigidbody[] SpawnedObjects;
+    int i;
     // Start is called before the first frame update
     void Start()
     {
+        i = 0;
+        SpawnedObjects = new Rigidbody[3];
         firing = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.GetComponent<Player>().playerOne && firing<3)
+        
+        if (gameObject.GetComponent<Player>().playerOne)
         {
+            Debug.Log(SpawnedObjects[0]);
+            Debug.Log(SpawnedObjects[1]);
+            Debug.Log(SpawnedObjects[2]);
             if (Input.GetKeyUp(KeyCode.LeftShift))
             {
                 Fire();
 
             }
         }
-        if (!gameObject.GetComponent<Player>().playerOne && firing < 3)
+        if (!gameObject.GetComponent<Player>().playerOne)
         {
             if (Input.GetKeyUp(KeyCode.RightShift))
             {
@@ -40,12 +48,27 @@ public class Shoot : MonoBehaviour
     {
         // keeps track of certain firing rate at a given time.
         firing++;
+        if(firing <= 3)
+        {
+            Rigidbody shellInstance =
+           Instantiate(shell, fire.position, fire.rotation) as Rigidbody;
+            SpawnedObjects[i % 3] = shellInstance;
+            shellInstance.velocity = 50 * fire.forward;
+        }
+        else
+        {
+            SpawnedObjects[i % 3].position = fire.position;
+            SpawnedObjects[i % 3].rotation = fire.rotation;
+            SpawnedObjects[i % 3].velocity = 50 * fire.forward;
 
+        }
         // Create an instance of the shell and store a reference to it's rigidbody.
-        Rigidbody shellInstance =
-            Instantiate(shell, fire.position, fire.rotation) as Rigidbody;
+        
+        i = i%3;
+        i++;
+        
         // Set the shell's velocity to the launch force in the fire position's forward direction.
-        shellInstance.velocity = 50 * fire.forward;
+        
     }
 }
 
